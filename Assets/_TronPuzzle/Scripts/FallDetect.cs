5ba3bc383779fb92d1d574823f8d0ac9cab97cd0
Rahul Joshi <rahuljoshi@172-18-179-13.DYNAPOOL.NYU.EDU>
2017-03-09 21:29:37 -0500

    Add fall detection. Fade to back and teleport disk and player back to start.

---
+++ Assets/_TronPuzzle/Scripts/FallDetect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class FallDetect : MonoBehaviour {
+
+	[SerializeField] GameObject player;
+	[SerializeField] GameObject disk;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.name == "BodyCollider") {
+			SteamVR_Fade.Start (Color.black, 1f);
+			player.transform.position = Vector3.zero;
+			disk.transform.position = Vector3.zero;
+		}
+	}
+}
